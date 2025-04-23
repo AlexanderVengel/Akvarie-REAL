f14d06af559e4f10561e0f427b9250ec06b40331
Alexander Vengel <next16273@edu.nextkbh.dk>
2025-04-23 08:49:00 +0200

    Superfisk test

--- Akvarie/Akvarie/sketch.js
+++ Akvarie/Akvarie/sketch.js
@@ -13,10 +13,9 @@ function setup()
     velVec = new Vector(2,2.5)
     sharkposvec = new Vector(2,1)
     sharkvelvec = new Vector(3,4)
-    dariusvelvec= new Vector(1,2)
     nemo = new Klovnefisk(posVec,velVec)
     haj = new Shark(sharkposvec,sharkvelvec)
-    darius = new Klovnefisk(posVec,dariusvelvec)
+   
 }
 
 function draw()
@@ -26,7 +25,5 @@ function draw()
     nemo.update()
     haj.show("Darkblue")
     haj.update()
-    darius.show("red")
-    darius.update()
+    
 }
-//test fn igen igen igen pls virk
