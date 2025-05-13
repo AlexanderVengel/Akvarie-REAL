b103cea81e8ace74d7bafe1f38cef933421bef6d
Labib <next16267@edu.nextkbh.dk>
2025-05-13 18:20:11 +0200

    github check

--- Akvarie/Akvarie/LabibFisk.js
+++ Akvarie/Akvarie/LabibFisk.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Sild {
     #pos; #vel; #sizeX; #sizeY;
     constructor(pos, vel) {
@@ -7,7 +6,7 @@ class Sild {
         this.#sizeX = 60;
         this.#sizeY = 30;
     }
-
+ 
     update() {
         this.#pos = this.#pos.add(this.#vel);
         this.boundaryCheck();
@@ -34,42 +33,3 @@ class Sild {
         }
     }
 }
-=======
-    class Sild{
-        #pos; #vel; #sizeX; #sizeY
-        constructor(pos,vel){
-            this.#pos = pos;
-            this.#vel = vel;
-            this.#sizeX = 60;
-            this.#sizeY = 30;
-        }
-     
-        update(){
-            this.#pos=this.#pos.add(this.#vel)
-            this.boundaryCheck()
-        }
-     
-        show(col="yellow"){
-            fill(col)
-            triangle(this.#pos.x, this.#pos.y,
-                    this.#pos.x-this.#sizeX+10, this.#pos.y-this.#sizeY/2,
-                    this.#pos.x-this.#sizeX+10, this.#pos.y+this.#sizeY/2);
-            ellipse(this.#pos.x,this.#pos.y,this.#sizeX,this.#sizeY)
-            fill("blue")
-            rect(this.#pos.x-this.#sizeX/8, this.#pos.y-this.#sizeY/2,this.#sizeX/8, this.#sizeY)
-            line(this.#pos.x,this.#pos.y,this.#pos.x+this.#sizeX/2,this.#pos.y)
-            circle(this.#pos.x+this.#sizeX/4,this.#pos.y-this.#sizeY/4,3)
-        }
-     
-        boundaryCheck(){
-       
-            if ((this.#pos.x > width) || (this.#pos.x < 0)) {
-                this.#vel.x = this.#vel.x * -1;
-            }
-            if ((this.#pos.y > height) || (this.#pos.y < 0)) {
-                this.#vel.y = this.#vel.y * -1;
-            }
-        }
-    }
-     
->>>>>>> 3b0d2859b07021664534334d15a421b5c25855f0
